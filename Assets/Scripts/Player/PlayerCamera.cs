571935822ff3505570571b18222727638a172367
Valeria Arenas <110551979+Varenas21@users.noreply.github.com>
2024-11-21 01:37:23 -0700

    Initial Commit
    Contains testing scene, character movement and needed textures to prototype

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    // Sensitivity
+    public float sensY;
+    public float sensX;
+
+    public Transform orientation;
+
+    float xRotation, yRotation;
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    public void Update()
+    {
+        // MOUSE INPUT
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate camera and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed,groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask isGround;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput, verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody> ();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        // Ground Check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, isGround);
+
+        UserInput(); 
+        SpeedControl();
+  
+
+        // Handle Drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else rb.drag = 0;
+
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void UserInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        // Calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //Limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+}
